655882dded3f46ba5be0193786f42128d338c595
Guilherme <guilhermesamt64@gmail.com>
2025-12-31 16:34:47 -0400

    small changes

--- Basics/Neuron2.py
+++ Basics/Neuron2.py
@@ -16,7 +16,7 @@ Matrix_of_ones = np.ones((3,1)) #Bias, PERMITE O AJUSTE DE DADOS , LHE DANDO FLE
 learning_rate = 0.01
 iterations = 100
 
-Peso = np.random.randn(4,3) #Sinal para ajustar na saida final do sinal, de cada neurônio, O TAMANHO DEVE SER A OPOSTA DE FEATURES CONCATENADA COM BIAS(BIAS CONCATENADA COM FEATURES TAMANHO IGUAL A 3X4), PESO DEVE SER 4X3.
+# Peso = np.random.randn(4,3) #Sinal para ajustar na saida final do sinal, de cada neurônio, O TAMANHO DEVE SER A OPOSTA DE FEATURES CONCATENADA COM BIAS(BIAS CONCATENADA COM FEATURES TAMANHO IGUAL A 3X4), PESO DEVE SER 4X3.
 Features_and_ones = np.concatenate((Features, Matrix_of_ones), axis=1) #Ao juntar Bias com Features, criamos um ajuste de dados na hora de realizar a função linear.
 
 #Soma ponderada é Z = W * X + B, tal que B=Bias, X=Features, W=Pesos.
@@ -25,9 +25,8 @@ Features_and_ones = np.concatenate((Features, Matrix_of_ones), axis=1) #Ao junta
 
 
 class Execução():
-    def __init__(self, Features_and_ones, Peso, Target, learning_rate, iterations):
+    def __init__(self, Features_and_ones, Target, learning_rate, iterations):
         self.Features_and_ones = Features_and_ones
-        self.Peso = Peso
         self.Target = Target
         self.learning_rate = learning_rate
         self.iterations = iterations
@@ -39,69 +38,71 @@ class Execução():
         self.W2 = np.random.randn(3,3)
 
     
-    def soma_ponderada(self):
-        self.Z = self.Features_and_ones @ self.Peso #@ é a multiplicação de matrizes, de colunas para linhas e vice-versa.
-        return self.Z
+    #def soma_ponderada(self):
+       # Z = Features_and_ones @ self.Peso #@ é a multiplicação de matrizes, de colunas para linhas e vice-versa.
+        #return Z
 
     def sigmoide(self, valor_Z):  #Função da sigmóide é de O(z) = 1/(1+e^-z)         
-        self.sigmoide_function = 1/(1+np.exp(-np.clip(valor_Z, -500, 500)))   #np.exp(valor) pegamos o número de euler e elevamos a um valor variável
-        return  self.sigmoide_function
+        sigmoide_function = 1/(1+np.exp(-np.clip(valor_Z, -500, 500)))   #np.exp(valor) pegamos o número de euler e elevamos a um valor variável
+        return  sigmoide_function
 
-    def foward_function(self):
+    def foward_function(self, W1, W2):
         #A fórmula do foward function é denotada por: A2 = sigmoide de (concatenar com(sigmodie de(X * W1) * W2))  
-        self.Z1 = self.Features_and_ones @ self.W1 #Multiplicamos a entrada dos dados já com o bias embutido com o W1 Nota Z1 = soma ponderada número 1
-        self.A1 = self.sigmoide(self.Z1) #A1 = ativação número 1 com base no valor de Z1 que obtemos, colocando Z1 na sigmoide
-        self.A1_with_bias = np.concatenate((self.A1, self.matiz_de_um), axis=1) #concatenando com os bias intermediário junto com A1, agora temos a ativação com bias
+        Z1 = self.Features_and_ones @ W1 #Multiplicamos a entrada dos dados já com o bias embutido com o W1 Nota Z1 = soma ponderada número 1
+        A1 = self.sigmoide(Z1) #A1 = ativação número 1 com base no valor de Z1 que obtemos, colocando Z1 na sigmoide
+        A1_with_bias = np.concatenate((A1, self.matiz_de_um), axis=1) #concatenando com os bias intermediário junto com A1, agora temos a ativação com bias
 
-        self.Z2 = self.A1_with_bias @ self.W2 #Multiplicamos os valores de ativação número 1 com o W2, criando assim uma nova entrada de dados denominada de Z2
-        self.A2 = self.sigmoide(self.Z2) #A2 = ativação número 2. *Nota, não é necessário o bias*
+        Z2 = A1_with_bias @ W2 #Multiplicamos os valores de ativação número 1 com o W2, criando assim uma nova entrada de dados denominada de Z2
+        A2 = self.sigmoide(Z2) #A2 = ativação número 2. *Nota, não é necessário o bias*
+        return A2, A1_with_bias
 
-    def backward_function(self):
+    def backward_function(self, A1, A2):
         #Cálculo do delta de saída é dado pela fórmula: S= sigma  S2 = (A2 - Y) * (A2 * (1 - A2))
-        self.delta_saida2 = (self.A2 - self.Target) * (self.A2 * (1 - self.A2))
+        delta_saida2 = (A2 - self.Target) * (A2 * (1 - A2))
 
-        self.delta_saida1 = self.delta_saida2 @ self.W2[:-1,:].T * (self.A1 * (1 - self.A1))
-        print(self.delta_saida1)
+        delta_saida1 = delta_saida2 @ self.W2[:-1,:].T * (A1 * (1 - A1))
+        return delta_saida1, delta_saida2
 
-
-    def funcao_de_custo(self, sigmoide_value):
+    def funcao_de_custo(self, A2):
         #função de custo, Entropia Cruzada Binária. Fórmula é J = (-1/m) Somatório [Y*log(A)+(1-Y)*log(1-A)] Tal que somatório=média
         #Com as hidden layers fazemos o seguinte, a fórmula que temos agora é J = (-1/m) somatório de [Y*Log(A2)+(1-Y)*Log(A2)]
         self.m = len(Target)
-        self.equacao = -np.mean(Target * np.log(self.A2 + self.epsilon) + (1 - Target) * np.log(1 - self.A2 + self.epsilon))
+        self.equacao = -np.mean(Target * np.log(A2) + (1 - self.Target) * np.log(1 - A2 + self.epsilon))
         return self.equacao
 
 
-    def gradiente_descendente(self, sigmoide_value):
+    def gradiente_descendente(self, delta_saida1, delta_saida2, A1_with_bias):
         #fórmula da derivada do gradiente descendente. dW = 1/m * X transposta * (A - Y)
         #Para as hidden layers alteramos um aspecto, no lugar de X e (A - Y) usamos os novos valores de A1 com bias embutido no lugar de X e a saida de delta no lugar de (A-Y)
-        self.gradient_descendent = 1/self.m * np.transpose(self.A1_with_bias) @ (self.delta_saida)
-        return self.gradient_descendent
+        self.gradient_descendent_W2 = 1/self.m * np.transpose(A1_with_bias) @ (delta_saida2) #Gradiente para pponte final de W2
 
-    def ativar_funcao(self):
-        self.foward_function()
-        self.funcao_de_custo(self.sigmoide_function)
-        self.backward_function()
-        self.gradiente_descendente(self.sigmoide_function)
+        self.gradient_descendent_W1 = 1/self.m * np.transpose(self.Features_and_ones) @ (delta_saida1)# Gradiente para a primeira ponte de W1
+
+    def new_weights(self):
+        self.W1 -= learning_rate * self.gradient_descendent_W1
+        self.W2 -= learning_rate * self.gradient_descendent_W2
 
 
-    #def TREINAMENTO(self):
+    def TREINAMENTO(self):
         for i in range(self.iterations):
-            valor_Z = self.soma_ponderada()
-            sigmoide_value = self.sigmoide(valor_Z)
-            valor_de_custo = self.funcao_de_custo(sigmoide_value)
+            pesos_value = self.foward_function(self.W1, self.W2)
+            valor_de_custo = self.funcao_de_custo(pesos_value)
             self.cost_history.append(valor_de_custo)
-            valor_do_gradiente = self.gradiente_descendente(sigmoide_value)
-            New_peso = self.Peso - learning_rate * valor_do_gradiente
+            backward_results = self.backward_function(pesos_value)
+            valor_do_gradiente = self.gradiente_descendente(backward_results, pesos_value)
+            new_weights = self.new_weights()
+            New_peso = new_weights
             self.Peso = New_peso
         return valor_de_custo, New_peso
 
 
-EXECUTAR = Execução(Features_and_ones, Peso, Target, learning_rate, iterations) 
-EXECUTAR.ativar_funcao()
+EXECUTAR = Execução(Features_and_ones, Target, learning_rate, iterations) 
+EXECUTAR.TREINAMENTO()
 
 valores_de_custos_totais, valores_do_peso_novo = EXECUTAR.TREINAMENTO()
+print(valores_de_custos_totais)
 print("///////////////////////////////////")
+print(valores_do_peso_novo)
 
 
 #W1 e W2. W1 é um conjunto de pesos que são misturados com os dados de entradas, modificando-os e transformando em 2 neurônios ocultos
